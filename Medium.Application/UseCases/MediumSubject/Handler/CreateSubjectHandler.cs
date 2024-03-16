9a7591d57cc3e4d5d43693702739df51e2fd0518
Husanboy <samadovxusan9013@gmail.com>
2024-03-16 21:35:21 +0500

    add

--- Medium.Application/UseCases/MediumSubject/Handler/CreateSubjectHandler.cs
+++ Medium.Application/UseCases/MediumSubject/Handler/CreateSubjectHandler.cs
@@ -2,6 +2,7 @@
 using MediatR;
 using Medium.Application.Absatractions;
 using Medium.Application.UseCases.MediumUser.Commands;
+using Medium.Domain.Entities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -20,9 +21,17 @@ namespace Medium.Application.UseCases.MediumSubject.Handler
             _mapper= mapper;
         }
 
-        public Task<Unit> Handle(CreateUserCommand request, CancellationToken cancellationToken)
+        public async Task<Unit> Handle(CreateUserCommand request, CancellationToken cancellationToken)
         {
+            var result = _mapper.Map<Subject>(request);
 
+            if (result is not null)
+            {
+                _applicationDbContext.Subjects.Add(result);
+                await _applicationDbContext.SaveChangesAsync();
+                return result;
+
+            }
         }
     }
 }
